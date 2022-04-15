bbd88e5d119d3fb07fcb457ceb4220cd36905101
Mathys J Taljaard <mathysjtaljaard@users.noreply.github.com>
2022-04-15 21:43:39 +0000

    Create basic QR code entry screen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import QRGenerator from './qr/QRGenerator';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <QRGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/qr/QRGenerator.js
@@ -0,0 +1,44 @@
+import { Fragment, useState } from "react"
+import QRCode from "react-qr-code"
+import { Container, Row, Col, Form } from "react-bootstrap"
+
+export default function QRGenerator() {
+
+    const [qrcodeValue, setQrcodeValue] = useState('')
+
+    const onChangeHandler = ({ target }) => {
+        setQrcodeValue(target.value);
+    }
+
+    return (
+        <Fragment >
+            <Container>
+                <Container  >
+                    <Row>
+                        <Col xs={4}>
+                            <Form.Group>
+                                <Form.Label>Enter URL</Form.Label>
+                                <Form.Control type="text" placeholder="enter value here" onChange={onChangeHandler} />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>&nbsp;</Row>
+                </Container>
+                <Container>
+                    <Row>&nbsp;</Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <QRCode value={qrcodeValue} />
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>&nbsp;</Row>
+                </Container>
+            </Container>
+
+        </Fragment>
+    )
+}
