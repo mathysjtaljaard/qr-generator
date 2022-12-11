0507589f5cfc01f7a65fb69bb010545a9d08a339
Mathys Taljaard <mathysjtaljaard@users.noreply.github.com>
2022-12-11 14:55:54 -0500

    Update to use webpack and module fed

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const isProduction = process.env.NODE_ENV === "production";
+const deps = require('./package.json').dependencies;
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+    port: 3002
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+
+    new MiniCssExtractPlugin(),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    new ModuleFederationPlugin({
+      name: "qrcode",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {
+        // NOTE: The Name on the left NEED to match the name of the file on the right!!
+        "./QRGenerator": "./src/qr/QRGenerator",
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+        bootstrap: {
+          singleton: true,
+          requiredVersion: deps.bootstrap,
+        },
+        "react-bootstrap": {
+          singleton: true,
+          requiredVersion: deps["react-bootstrap"]
+        }
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
